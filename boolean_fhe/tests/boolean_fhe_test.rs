3adaeb8e6973a8a85cce58c9f948e2877646077e
serendipity-crypto <serendipity_crypto@qq.com>
2024-01-08 19:10:00 +0800

    update boolean fhe test

--- boolean_fhe/tests/boolean_fhe_test.rs
+++ boolean_fhe/tests/boolean_fhe_test.rs
@@ -14,37 +14,24 @@ fn test_nand() {
 
     let evk = EvaluationKey::new(&skp);
 
-    let mut m = rng.gen();
-    let mut c = skp.encrypt(m);
+    // encrypt
+    let m0 = rng.gen();
+    let c0 = skp.encrypt(m0);
 
-    for i in 0..4 {
-        let m0 = rng.gen();
-        let c0 = skp.encrypt(m0);
+    let m1 = rng.gen();
+    let c1 = skp.encrypt(m1);
 
-        let m1 = rng.gen();
-        let c1 = skp.encrypt(m1);
+    // nand
+    let c2 = evk.nand(&c0, &c1);
 
-        let c2 = evk.nand(&c0, &c1);
-        let (m2, noise) = skp.decrypt_with_noise(&c2);
+    // decrypt
+    let (m2, noise) = skp.decrypt_with_noise(&c2);
 
-        assert_eq!(m2, !(m0 & m1));
-        println!(
-            "Noise: {}",
-            DefaultRing100::MODULUS_F64 / 16.0 - noise.as_f64()
-        );
-        assert!(noise.as_f64() <= DefaultRing100::MODULUS_F64 / 16.0);
-
-        c = evk.nand(&c, &c2);
-
-        let (d, noise) = skp.decrypt_with_noise(&c);
-        assert_eq!(d, !(m & m2));
-        println!(
-            "Noise: {}",
-            DefaultRing100::MODULUS_F64 / 16.0 - noise.as_f64()
-        );
-        assert!(noise.as_f64() <= DefaultRing100::MODULUS_F64 / 16.0);
-
-        m = d;
-        println!("The {i} nand test done!\n");
-    }
+    // check
+    assert_eq!(m2, !(m0 & m1));
+    // println!(
+    //     "Noise: {}",
+    //     DefaultRing100::MODULUS_F64 / 16.0 - noise.as_f64()
+    // );
+    assert!(noise.as_f64() <= DefaultRing100::MODULUS_F64 / 16.0);
 }
