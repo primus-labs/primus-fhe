4fefac38a4f5571a239ac53a294cdd5e52aa787d
serendipity-crypto <serendipity_crypto@qq.com>
2024-02-24 12:18:11 +0800

    modify some macros

--- algebra/src/reduce/mod.rs
+++ algebra/src/reduce/mod.rs
@@ -8,13 +8,13 @@ pub use ops::*;
 
 /// A helper trait to get the modulus of the field.
 pub trait ModulusConfig {
-    /// Barrett Modulus type
+    /// Modulus type
     type Modulus;
 
     /// The modulus of the field.
     const MODULUS: Self::Modulus;
 
-    /// Get the barrett modulus of the field.
+    /// Get the modulus of the field.
     #[inline]
     fn modulus() -> Self::Modulus {
         Self::MODULUS
