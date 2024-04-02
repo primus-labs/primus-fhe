99a8b0cfc63f4f118205c2b7217117dd022b55da
serendipity-crypto <serendipity_crypto@qq.com>
2024-04-02 14:58:51 +0800

    add benches

--- boolean_fhe/benches/he_bench.rs
+++ boolean_fhe/benches/he_bench.rs
@@ -22,8 +22,35 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let m1 = rng.gen();
     let c1 = skp.encrypt(m1);
 
+    let m2 = rng.gen();
+    let c2 = skp.encrypt(m2);
+
+    c.bench_function("not", |b| b.iter(|| evk.not(black_box(&c0))));
+
+    c.bench_function("and", |b| {
+        b.iter(|| evk.and(black_box(&c0), black_box(&c1)))
+    });
+
     c.bench_function("nand", |b| {
         b.iter(|| evk.nand(black_box(&c0), black_box(&c1)))
+    });
+
+    c.bench_function("or", |b| b.iter(|| evk.or(black_box(&c0), black_box(&c1))));
+
+    c.bench_function("nor", |b| {
+        b.iter(|| evk.nor(black_box(&c0), black_box(&c1)))
+    });
+
+    c.bench_function("xor", |b| {
+        b.iter(|| evk.xor(black_box(&c0), black_box(&c1)))
+    });
+
+    c.bench_function("xnor", |b| {
+        b.iter(|| evk.xnor(black_box(&c0), black_box(&c1)))
+    });
+
+    c.bench_function("majority", |b| {
+        b.iter(|| evk.majority(black_box(&c0), black_box(&c1), black_box(&c2)))
     });
 }
 
