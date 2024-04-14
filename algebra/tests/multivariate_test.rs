d75ef1961d27497c3c34605c569f7417dc84125a
f7ed <fred_include@outlook.com>
2024-04-14 19:37:04 +0800

    allow complex type

--- algebra/tests/multivariate_test.rs
+++ algebra/tests/multivariate_test.rs
@@ -134,9 +134,7 @@ fn evaluate_lists_of_products_with_op_at_a_point() {
         .map(|x| Rc::new(DenseMultilinearExtension::from_evaluations_vec(nv, x)))
         .collect();
     let coeff = FF::new(4);
-    let mut op_coefficient: Vec<(FF, FF)> = Vec::with_capacity(2);
-    op_coefficient.push((FF::new(2), FF::new(0)));
-    op_coefficient.push((FF::new(1), FF::new(3)));
+    let op_coefficient = vec![(FF::new(2), FF::new(0)), (FF::new(1), FF::new(3))];
     // coeff \cdot [2f \cdot (g + 3)]
     poly.add_product_with_op(products, &op_coefficient, coeff);
     // 4 * [2*2 * (2+3)] = 80
