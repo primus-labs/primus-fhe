b614ba9e5d1fce8e206cdad5d2fc3ddd12fcb4bb
f7ed <fred_include@outlook.com>
2024-04-24 09:29:23 +0800

    restructure

--- protocol/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![deny(missing_docs)]
-
-//! Define arithmetic operations.
-pub mod bit_decomposition;
-pub mod sumcheck;
-
-pub use error::Error;
-mod error;
