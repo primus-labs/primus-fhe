96f64f7b6682fb09decc9ccfcc41f018d1cbb2c3
serendipity-crypto <serendipity_crypto@qq.com>
2024-05-11 15:04:42 +0800

    improve extract during bootstrapping

--- boolean_ntru_fhe/src/key_switching_key.rs
+++ boolean_ntru_fhe/src/key_switching_key.rs
@@ -1,14 +1,14 @@
-use algebra::{FieldDiscreteGaussianSampler, NTTField, NTTPolynomial, Polynomial};
-use lattice::{DecompositionSpace, NTTGadgetRLWE, LWE, NTTRLWE, RLWE};
+use algebra::{FieldDiscreteGaussianSampler, NTTField, Polynomial};
+use lattice::{DecompositionSpace, NTTGadgetRLWE, PolynomialSpace, LWE, NTTRLWE, RLWE};
 use rand::{CryptoRng, Rng};
 
-use crate::{LWEPlaintext, SecretKeyPack};
+use crate::{LWEPlaintext, NTRUCiphertext, SecretKeyPack};
 
 /// The Key Switching Key.
 ///
 /// This struct stores the key
-/// to switch a [`LWE`] ciphertext from the RLWE Secret Key
-/// to a [`LWE`] ciphertext of the LWE Secret Key.
+/// that switch a [`NTRUCiphertext<F>`] ciphertext of the NTRU Secret Key
+/// to a [`LWE<F>`] ciphertext of the LWE Secret Key.
 pub struct KeySwitchingKey<F: NTTField> {
     /// LWE vector dimension, refers to **`n`** in the paper.
     lwe_dimension: usize,
@@ -18,31 +18,27 @@ pub struct KeySwitchingKey<F: NTTField> {
 
 impl<F: NTTField> KeySwitchingKey<F> {
     /// Performs key switching operation.
-    pub fn key_switch(&self, ciphertext: LWE<F>) -> LWE<F> {
+    pub fn key_switch(&self, ciphertext: &NTRUCiphertext<F>) -> LWE<F> {
         let n = self.lwe_dimension;
-        let a: Vec<Polynomial<F>> = ciphertext
-            .a()
-            .chunks_exact(n)
-            .map(|a| {
-                let mut p: Vec<F> = a.iter().map(|&x| -x).collect();
-                p[0] = -p[0];
-                p[1..].reverse();
-                <Polynomial<F>>::new(p)
-            })
-            .collect();
 
-        let mut init = <NTTRLWE<F>>::new(
-            NTTPolynomial::zero(n),
-            NTTPolynomial::new(vec![ciphertext.b(); n]),
-        );
+        // Because the lwe ciphertext extracted from a ntru ciphertext always has `b = 0`.
+        let mut init = <NTTRLWE<F>>::zero(n);
 
+        let mut polynomial_space = PolynomialSpace::new(n);
         let mut decompose_space = DecompositionSpace::new(n);
 
-        self.key.iter().zip(a).for_each(|(k_i, a_i)| {
-            init.sub_assign_gadget_rlwe_mul_polynomial_inplace_fast(k_i, a_i, &mut decompose_space);
+        let a_iter = ciphertext.as_slice().chunks_exact(n);
+
+        self.key.iter().zip(a_iter).for_each(|(k_i, a_i)| {
+            polynomial_space.copy_from(a_i);
+            init.add_assign_gadget_rlwe_mul_polynomial_inplace_fast(
+                k_i,
+                &mut polynomial_space,
+                &mut decompose_space,
+            );
         });
 
-        <RLWE<F>>::from(init).extract_lwe_locally()
+        <RLWE<F>>::from(init).extract_lwe_reverse_locally()
     }
 }
 
@@ -60,30 +56,41 @@ impl<F: NTTField> KeySwitchingKey<F> {
         let lwe_dimension = parameters.lwe_dimension();
         let key_switching_basis = parameters.key_switching_basis();
 
+        // negative convertion
         let convert = |v: &LWEPlaintext| match *v {
             0 => F::ZERO,
-            1 => F::ONE,
-            _ => F::NEG_ONE,
+            1 => F::NEG_ONE,
+            _ => F::ONE,
         };
 
-        let s = <Polynomial<F>>::new(
+        // s = [s_0, -s_{n-1},..., -s_1]
+        let mut s = <Polynomial<F>>::new(
             secret_key_pack
                 .lwe_secret_key()
                 .iter()
                 .map(convert)
                 .collect(),
         );
+        s[0] = -s[0];
+        s[1..].reverse();
 
         let s = s.into_ntt_polynomial();
 
         let len = key_switching_basis.decompose_len();
 
-        let key = secret_key_pack
-            .ring_secret_key()
-            .as_slice()
+        // f = [f_0, -f_{N-1},..., -f_1]
+        let mut key = secret_key_pack.ring_secret_key().as_slice().to_vec();
+        key[0] = -key[0];
+        key[1..].reverse();
+
+        let key = key
             .chunks_exact(lwe_dimension)
             .map(|z| {
-                let mut ntt_z = Polynomial::from_slice(z).into_ntt_polynomial();
+                let mut p = Polynomial::from_slice(z);
+                p[0] = -p[0];
+                p[1..].reverse();
+
+                let mut ntt_z = p.into_ntt_polynomial();
                 let k_i: Vec<NTTRLWE<F>> = (0..len)
                     .map(|i| {
                         let mut sample =
