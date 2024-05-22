ccf112d0861c968a1d4caedb42094148b99dcacf
fred <40384421+f7ed@users.noreply.github.com>
2024-05-22 16:05:53 +0800

    Add Bit Decomposition (#87)
    * add Sumcheck Protocol
    
    * rebase
    
    * refine the usage of uniform sampler
    
    * support linear op over the MLE stored in ListsOfProductsPolynomials
    
    * support linear op over the MLE stored in ListsOfProductsPolynomials
    
    * fix a bug
    
    * allow complex type
    
    * add comments
    
    * rename to add_product_with_linear_op
    
    * add bit_decomposition
    
    * rewrite add_product with invoking add_product_with_linear_op
    
    * minor comments + remove clone()
    
    * add comments for bit decomposition
    
    * change Vec<_> to Vec<Rc<_>> in verify_subclaim
    
    * add decomposition for mle
    
    * restructure
    
    * add zkp: sumcheck + bit decomposition
    
    * remove poly in DecomposedBits
    
    * add inline
    
    * minor changes
    
    * minor
    
    * minor

--- algebra/tests/multivariate_test.rs
+++ algebra/tests/multivariate_test.rs
@@ -2,10 +2,10 @@ use std::vec;
 
 use algebra::{
     derive::{Field, Prime},
-    DenseMultilinearExtension, Field, FieldUniformSampler, ListOfProductsOfPolynomials,
+    Basis, DenseMultilinearExtension, Field, FieldUniformSampler, ListOfProductsOfPolynomials,
     MultilinearExtension,
 };
-use num_traits::Zero;
+use num_traits::{Pow, Zero};
 use rand::thread_rng;
 use rand_distr::Distribution;
 use std::rc::Rc;
@@ -27,7 +27,7 @@ pub struct Fp32(u32);
 type FF = Fp32;
 type PolyFf = DenseMultilinearExtension<FF>;
 
-fn evaluate_mle_data_arry<F: Field>(data: &[F], point: &[F]) -> F {
+fn evaluate_mle_data_array<F: Field>(data: &[F], point: &[F]) -> F {
     if data.len() != (1 << point.len()) {
         panic!("Data size mismatch with number of variables.")
     }
@@ -60,7 +60,7 @@ fn evaluate_mle_at_a_random_point() {
     let point: Vec<_> = (0..2).map(|_| uniform.sample(&mut rng)).collect();
     assert_eq!(
         poly.evaluate(&point),
-        evaluate_mle_data_arry(&poly.evaluations, &point),
+        evaluate_mle_data_array(&poly.evaluations, &point),
     );
 }
 
@@ -105,7 +105,49 @@ fn mle_arithmetic() {
             assert_eq!(&poly1 + &PolyFf::zero(), poly1);
             assert_eq!((&PolyFf::zero() + &poly1), poly1);
         }
+
+        // test decomposition of mle
+        {
+            let base_len = 3;
+            let base = FF::new(1 << base_len);
+            let basis = <Basis<FF>>::new(base_len);
+            let bits_len = basis.decompose_len();
+            let decomposed_polys = poly1.get_decomposed_mles(base_len, bits_len as u32);
+            let point: Vec<_> = (0..NV).map(|_| uniform.sample(&mut rng)).collect();
+            let evaluation = decomposed_polys
+                .iter()
+                .enumerate()
+                .fold(FF::ZERO, |acc, (i, bit)| {
+                    acc + bit.evaluate(&point) * base.pow(i as u32)
+                });
+            assert_eq!(poly1.evaluate(&point), evaluation);
+        }
     }
+}
+
+#[test]
+fn trivial_decomposed_mles() {
+    let base_len = 2; // i.e. base = 4
+    let base = FF::new(1 << base_len);
+    let bits_len = 3;
+    let num_vars = 2;
+
+    let val = field_vec!(FF; 0b001101, 0b100011, 0b101100, 0b111110);
+    let poly = DenseMultilinearExtension::from_evaluations_vec(num_vars, val);
+    let decomposed_polys = poly.get_decomposed_mles(base_len, bits_len);
+
+    let uniform = <FieldUniformSampler<FF>>::new();
+    let point: Vec<_> = (0..num_vars)
+        .map(|_| uniform.sample(&mut thread_rng()))
+        .collect();
+    let evaluation = decomposed_polys
+        .iter()
+        .enumerate()
+        .fold(FF::ZERO, |acc, (i, bit)| {
+            acc + bit.evaluate(&point) * base.pow(i as u32)
+        });
+
+    assert_eq!(poly.evaluate(&point), evaluation);
 }
 
 #[test]
@@ -122,4 +164,22 @@ fn evaluate_lists_of_products_at_a_point() {
 
     let point = field_vec!(FF; 0, 1);
     assert_eq!(poly.evaluate(&point), FF::new(24));
+}
+
+#[test]
+fn evaluate_lists_of_products_with_op_at_a_point() {
+    let nv = 2;
+    let mut poly = ListOfProductsOfPolynomials::new(nv);
+    let products = vec![field_vec!(FF; 1, 2, 3, 4), field_vec!(FF; 1, 2, 3, 4)];
+    let products: Vec<Rc<DenseMultilinearExtension<FF>>> = products
+        .into_iter()
+        .map(|x| Rc::new(DenseMultilinearExtension::from_evaluations_vec(nv, x)))
+        .collect();
+    let coeff = FF::new(4);
+    let op_coefficient = vec![(FF::new(2), FF::new(0)), (FF::new(1), FF::new(3))];
+    // coeff \cdot [2f \cdot (g + 3)]
+    poly.add_product_with_linear_op(products, &op_coefficient, coeff);
+    // 4 * [2*2 * (2+3)] = 80
+    let point = field_vec!(FF; 1, 0);
+    assert_eq!(poly.evaluate(&point), FF::new(80));
 }
---
+++ zkp/src/utils.rs
@@ -0,0 +1,60 @@
+//! This module defines some useful utils that may invoked by piop.
+use algebra::{DenseMultilinearExtension, Field};
+
+/// Generate MLE of the ideneity function eq(u,x) for x \in \{0, 1\}^dim
+pub fn gen_identity_evaluations<F: Field>(u: &[F]) -> DenseMultilinearExtension<F> {
+    let dim = u.len();
+    let mut evaluations: Vec<_> = vec![F::ZERO; 1 << dim];
+    evaluations[0] = F::ONE;
+    for i in 0..dim {
+        // The index represents a point in {0,1}^`num_vars` in little endian form.
+        // For example, `0b1011` represents `P(1,1,0,1)`
+        let u_i_rev = u[dim - i - 1];
+        for b in (0..(1 << i)).rev() {
+            evaluations[(b << 1) + 1] = evaluations[b] * u_i_rev;
+            evaluations[b << 1] = evaluations[b] * (F::ONE - u_i_rev);
+        }
+    }
+    DenseMultilinearExtension::from_evaluations_vec(dim, evaluations)
+}
+
+/// Evaluate eq(u, v) = \prod_i (u_i * v_i + (1 - u_i) * (1 - v_i))
+pub fn eval_identity_function<F: Field>(u: &[F], v: &[F]) -> F {
+    assert_eq!(u.len(), v.len());
+    let mut evaluation = F::ONE;
+    for (u_i, v_i) in u.iter().zip(v) {
+        evaluation *= *u_i * *v_i + (F::ONE - *u_i) * (F::ONE - *v_i);
+    }
+    evaluation
+}
+
+#[cfg(test)]
+mod test {
+    use crate::utils::{eval_identity_function, gen_identity_evaluations};
+    use algebra::{
+        derive::{Field, Prime},
+        FieldUniformSampler, MultilinearExtension,
+    };
+    use rand::thread_rng;
+    use rand_distr::Distribution;
+
+    #[derive(Field, Prime)]
+    #[modulus = 132120577]
+    pub struct Fp32(u32);
+    // field type
+    type FF = Fp32;
+
+    #[test]
+    fn test_gen_identity_evaluations() {
+        let sampler = <FieldUniformSampler<FF>>::new();
+        let mut rng = thread_rng();
+        let dim = 10;
+        let u: Vec<_> = (0..dim).map(|_| sampler.sample(&mut rng)).collect();
+
+        let identity_at_u = gen_identity_evaluations(&u);
+
+        let v: Vec<_> = (0..dim).map(|_| sampler.sample(&mut rng)).collect();
+
+        assert_eq!(eval_identity_function(&u, &v), identity_at_u.evaluate(&v));
+    }
+}
