515c2e1618ffc3569525b7b713cb967043ce5c3f
Xiang Xie <xiexiangiscas@gmail.com>
2024-05-28 21:37:46 +0800

    add prg with aes-ni

--- algebra/src/utils/mod.rs
+++ algebra/src/utils/mod.rs
@@ -4,8 +4,16 @@ mod gcd;
 mod prime;
 mod reverse;
 mod sample;
+mod sse2neon;
+mod aes;
+mod block;
+mod prg;
 
 pub use gcd::*;
 pub use prime::*;
 pub use reverse::*;
 pub use sample::*;
+pub use sse2neon::*;
+pub use aes::*;
+pub use block::*;
+pub use prg::*;
