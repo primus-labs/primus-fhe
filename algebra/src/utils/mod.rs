584f7db3b4beee464d1122c9bf1689a1e2362b03
Xiang Xie <xiexiangiscas@gmail.com>
2024-05-30 17:10:05 +0800

    Fast RNG based on AES-NI (#98)
    * add prg with aes-ni
    
    * optimize and bench sample functions
    
    * replace chacha12 with prg in fhe_core
    
    * fmt

--- algebra/src/utils/mod.rs
+++ algebra/src/utils/mod.rs
@@ -1,11 +1,19 @@
 //! Implemention of some number theory operation.
 
+mod aes;
+mod block;
 mod gcd;
+mod prg;
 mod prime;
 mod reverse;
 mod sample;
+mod sse2neon;
 
+pub use aes::*;
+pub use block::*;
 pub use gcd::*;
+pub use prg::*;
 pub use prime::*;
 pub use reverse::*;
 pub use sample::*;
+pub use sse2neon::*;
