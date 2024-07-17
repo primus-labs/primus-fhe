9eb43aff2eccc1b0ad054cde0f41270f912e6edc
Xiang Xie <xiexiangiscas@gmail.com>
2024-07-17 14:02:35 +0800

    Refine Brakedown PCS (#116)
    * refator pcs
    
    * refactor brakedown pcs
    
    * clean up
    
    * add DP23 optimization
    
    * bench
    
    * remove hash pp
    
    * fmt
    
    * add example
    
    * minor
    
    * merge main

--- algebra/src/utils/transcript.rs
+++ algebra/src/utils/transcript.rs
@@ -1,6 +1,5 @@
 use std::marker::PhantomData;
 
-use merlin::Transcript;
 use rand::SeedableRng;
 use rand_distr::Distribution;
 use serde::Serialize;
@@ -9,27 +8,42 @@ use crate::{Field, FieldUniformSampler};
 
 use super::{Block, Prg};
 
-/// An IOP transcript consists of a Merlin transcript and a `sampler``
+/// A transcript consists of a Merlin transcript and a `sampler``
 /// to sample uniform field elements.
-pub struct IOPTranscript<F: Field> {
-    transcript: Transcript,
+pub struct Transcript<F: Field> {
+    transcript: merlin::Transcript,
     sampler: FieldUniformSampler<F>,
     _marker: PhantomData<F>,
 }
 
-impl<F: Field + Serialize> IOPTranscript<F> {
+impl<F: Field> Transcript<F> {
     /// Create a new IOP transcript.
+    #[inline]
     pub fn new() -> Self {
         Self {
-            transcript: Transcript::new(b""),
+            transcript: merlin::Transcript::new(b""),
             sampler: FieldUniformSampler::new(),
             _marker: PhantomData,
         }
     }
-
+}
+impl<F: Field + Serialize> Transcript<F> {
     /// Append the message to the transcript.
+    #[inline]
     pub fn append_message(&mut self, msg: &[u8]) {
         self.transcript.append_message(b"", msg);
+    }
+
+    /// Append elements to the transcript.
+    #[inline]
+    pub fn append_elements(&mut self, elems: &[F]) {
+        self.append_message(&bincode::serialize(elems).unwrap());
+    }
+
+    /// Generate the challenge bytes from the current transcript
+    #[inline]
+    pub fn get_challenge_bytes(&mut self, bytes: &mut [u8]) {
+        self.transcript.challenge_bytes(b"", bytes);
     }
 
     /// Generate the challenge from the current transcript
@@ -43,9 +57,22 @@ impl<F: Field + Serialize> IOPTranscript<F> {
 
         challenge
     }
+
+    /// Generate the challenge vector from the current transcript
+    /// and append it to the transcript.
+    pub fn get_vec_and_append_challenge(&mut self, num: usize) -> Vec<F> {
+        let mut seed = [0u8; 16];
+        self.transcript.challenge_bytes(b"", &mut seed);
+        let mut prg = Prg::from_seed(Block::from(seed));
+
+        let challenge = self.sampler.sample_iter(&mut prg).take(num).collect();
+        self.append_message(&bincode::serialize(&challenge).unwrap());
+
+        challenge
+    }
 }
 
-impl<F: Field> Default for IOPTranscript<F> {
+impl<F: Field> Default for Transcript<F> {
     fn default() -> Self {
         Self::new()
     }
--- pcs/src/utils/merkle_tree.rs
+++ pcs/src/utils/merkle_tree.rs
@@ -1,18 +1,31 @@
-use crate::utils::hash::Hash;
+use serde::{Deserialize, Serialize};
 
-/// Root of the Merkle Tree only
-#[derive(Debug, Clone, Default)]
+use crate::utils::hash::Hash;
+use bincode::Result;
+
+/// Root of the Merkle Tree
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct MerkleRoot<H: Hash> {
-    /// the depth of the merkle tree
+    /// The depth of the merkle tree
     pub depth: usize,
-    /// the root of the merkle tree
+    /// The root of the merkle tree
     pub root: H::Output,
 }
 
 impl<H: Hash> MerkleRoot<H> {
-    /// instantiate a merkle root
+    /// Instantiate a merkle root
     pub fn new(depth: usize, root: H::Output) -> Self {
         Self { depth, root }
+    }
+
+    /// Convert into bytes.
+    pub fn to_bytes(&self) -> Result<Vec<u8>> {
+        bincode::serialize(&self)
+    }
+
+    /// Recover from bytes.
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        bincode::deserialize(bytes)
     }
 }
 
@@ -25,47 +38,68 @@ pub struct MerkleTree<H: Hash> {
     pub root: H::Output,
     /// the merkle tree
     pub tree: Vec<H::Output>,
+    /// input size,
+    pub input_size: usize,
 }
 
 impl<H: Hash> MerkleTree<H> {
-    /// instantiate a merkle tree by committing the leaves
-    pub fn new(mut tree: Vec<H::Output>) -> Self {
-        // resize the size from leaves size to tree size
-        let depth = tree.len().next_power_of_two().ilog2() as usize;
-        let size = (1 << (depth + 1)) - 1;
-        tree.resize(size, H::Output::default());
+    /// Create a new instance.
+    pub fn new() -> Self {
+        Self {
+            depth: 0,
+            root: H::Output::default(),
+            tree: Vec::default(),
+            input_size: 0,
+        }
+    }
 
-        // merklize the leaves
+    /// Instantiate a merkle tree by committing the leaves
+    /// In this case, we assume all the input leafs as the hashed values.
+    ///
+    /// # Arguments.
+    ///
+    /// * `leaves` - The leaves used to generate the merkle tree.
+    pub fn generate(&mut self, leaves: &[H::Output]) {
+        // Resize the size from leaves size to tree size
+        self.input_size = leaves.len();
+        let depth = leaves.len().next_power_of_two().ilog2() as usize;
+        let size = (1 << (depth + 1)) - 1;
+        self.tree = vec![H::Output::default(); size];
+        self.tree[..leaves.len()].copy_from_slice(leaves);
+
+        // Merklize the leaves
         let mut hasher = H::new();
-        // use base to index the start of the lower layer
+
+        // Use base to index the start of the lower layer
         let mut base = 0;
         for depth in (1..=depth).rev() {
-            // view the lower layer as the input and the upper layer as its output
+            // View the lower layer as the input and the upper layer as its output
             let input_len = 1 << depth;
             let output_len = input_len >> 1;
             let (inputs, outputs) =
-                tree[base..base + input_len + output_len].split_at_mut(input_len);
-            // compute the output of the hash function given the input
+                self.tree[base..base + input_len + output_len].split_at_mut(input_len);
+
+            // Compute the output of the hash function given the input
             inputs
                 .chunks_exact(2)
                 .zip(outputs.iter_mut())
                 .for_each(|(input, output)| {
-                    hasher.update_hash_value(input[0]);
-                    hasher.update_hash_value(input[1]);
+                    hasher.update_hash_value(input[0].as_ref());
+                    hasher.update_hash_value(input[1].as_ref());
                     *output = hasher.output_reset();
                 });
             base += input_len;
         }
 
-        let root = *tree.last().unwrap();
-
-        Self { depth, root, tree }
+        self.depth = depth;
+        self.root = self.tree[size - 1];
     }
 
-    /// return merkle paths of the indexed leaf
+    /// Return merkle paths of the indexed leaf
     /// which consists of the leaf hash and neighbour hashes
     #[inline]
     pub fn query(&self, leaf_idx: usize) -> Vec<H::Output> {
+        assert!(leaf_idx <= self.input_size);
         let mut base = 0;
         let mut merkle_path: Vec<H::Output> = Vec::new();
         merkle_path.push(self.tree[leaf_idx]);
@@ -78,23 +112,23 @@ impl<H: Hash> MerkleTree<H> {
         merkle_path
     }
 
-    /// check whether the merkle path is consistent with the root
+    /// Check whether the merkle path is consistent with the root
     #[inline]
-    pub fn check(committed_root: H::Output, leaf_idx: usize, path: &[H::Output]) -> bool {
+    pub fn check(committed_root: &H::Output, leaf_idx: usize, path: &[H::Output]) -> bool {
         let mut hasher = H::new();
 
         let leaf = path[0];
         let path_root = path[1..].iter().enumerate().fold(leaf, |acc, (idx, hash)| {
             if (leaf_idx >> idx) & 1 == 0 {
-                hasher.update_hash_value(acc);
-                hasher.update_hash_value(*hash); ////?
+                hasher.update_hash_value(acc.as_ref());
+                hasher.update_hash_value(hash.as_ref());
             } else {
-                hasher.update_hash_value(*hash);
-                hasher.update_hash_value(acc);
+                hasher.update_hash_value(hash.as_ref());
+                hasher.update_hash_value(acc.as_ref());
             }
             hasher.output_reset()
         });
 
-        path_root == committed_root
+        path_root == *committed_root
     }
 }
