1ce542fb85811bae2e5f3905770bd509d57636dd
serendipity-crypto <serendipity_crypto@qq.com>
2024-08-07 17:02:27 +0800

    update

--- fhe_cmp/examples/cmp.rs
+++ fhe_cmp/examples/cmp.rs
@@ -1,10 +1,15 @@
-mod comparison;
-use algebra::{derive::{DecomposableField, FheField, Field, Prime, NTT},Basis,DecomposableField, Field,
-    Polynomial, NTTPolynomial};
-use lattice::{RGSW, NTTRLWE,RLWE};
+use algebra::{
+    derive::{DecomposableField, FheField, Field, Prime, NTT},
+    DecomposableField, Field, NTTPolynomial, Polynomial,
+};
+use fhe_cmp::comparison;
+use fhe_core::{
+    BlindRotationType, ConstParameters, ModulusSwitchRoundMethod, Parameters, RLWEBlindRotationKey,
+    RingSecretKeyType, SecretKeyPack, SecretKeyType, StepsAfterBR,
+};
+use lattice::{NTTRLWE, RGSW, RLWE};
+use num_traits::Zero;
 use rand::prelude::*;
-use fhe_core::{BlindRotationType, ConstParameters, ModulusSwitchRoundMethod, Parameters, RingSecretKeyType,
-    SecretKeyPack, SecretKeyType, StepsAfterBR,RLWEBlindRotationKey};
 
 #[derive(Field, Prime, DecomposableField, FheField, NTT)]
 #[modulus = 81720453121]
@@ -15,7 +20,9 @@ pub type NTTRingSecretKey<FF> = NTTPolynomial<FF>;
 type Inner = u64; // inner type
 
 const FP: Inner = 81720453121; // ciphertext space
-const FT: Inner = 8; // message space
+const FT: Inner = 16; // message space
+const DEELTA: FF = FF((FP as f64 / FT as f64) as Inner);
+const HALF_DEELTA: FF = FF((FP as f64 / (FT as f64 * 2.0)) as Inner);
 
 #[inline]
 fn encode(m: Inner) -> FF {
@@ -23,13 +30,18 @@ fn encode(m: Inner) -> FF {
 }
 
 #[inline]
+fn half_encode(m: Inner) -> FF {
+    FF::new((m as f64 * FP as f64 / (FT as f64 * 2.0)).round() as Inner)
+}
+
+#[inline]
 fn decode(c: FF) -> Inner {
     (c.value() as f64 * FT as f64 / FP as f64).round() as Inner % FT
 }
 
-fn main(){
+fn main() {
     let mut rng = thread_rng();
-    let param=Parameters::<u64, FF>::new(ConstParameters {
+    let param = Parameters::<u32, FF>::new(ConstParameters {
         lwe_dimension: 1024,
         lwe_modulus: 1024,
         t: 4,
@@ -47,41 +59,40 @@ fn main(){
         modulus_switcing_round_method: ModulusSwitchRoundMethod::Round,
     })
     .unwrap();
-    let sk =SecretKeyPack::new(param);
-    let value = 1;
-    let basis = <Basis<FF>>::new(3);
+
+    let sk = SecretKeyPack::new(param);
+    let basis = param.blind_rotation_basis();
     let sampler = param.ring_noise_distribution();
     let code = sk.ring_secret_key().as_slice();
-    let ntt_num1=NTTRLWE::generate_random_value_sample(
-        sk.ntt_ring_secret_key(),
-        encode(value),
-        sampler,
-        &mut rng
-    );
-    let num2 = RGSW::generate_random_one_sample(
-        &mut rng,
-        basis,
-        sampler,
-        sk.ntt_ring_secret_key()
-    );
-    let num1=RLWE::from(ntt_num1);
 
-    let in1_0=comparison::rlwe_turn(num1,1);
-    let in2_0=comparison::rgsw_turn(num2,0);
-    let in1_1=in1_0.clone();
-    let in2_1=in2_0.clone();   
-    let rotationkey = RLWEBlindRotationKey::generate(&sk,sampler,&mut rng);
-    let vec1 = vec![in1_0,in1_1];
-    let vec2 = vec![in2_0,in2_1];
-    let out1 = comparison::greater_arbhcmp_fixed(&vec1, &vec2,2,rotationkey);
-    let out1_a=out1.a();
-    let mut num1 = FF::new(0);
-    for i in 0..1024{
-        num1 = num1 +code[i]*out1_a[i];
-    }
-    let decoded_value1 = decode(out1.b()-num1);
+    let value = 1;
+    let ntt_num1 = NTTRLWE::generate_random_value_sample(
+        sk.ntt_ring_secret_key(),
+        half_encode(value),
+        sampler,
+        &mut rng,
+    );
+    let mut num1 = RLWE::from(ntt_num1);
+
+    let mut num2 =
+        RGSW::generate_random_one_sample(&mut rng, basis, sampler, sk.ntt_ring_secret_key());
+
+    comparison::rlwe_turn(&mut num1, 1);
+    comparison::rgsw_turn(&mut num2, 0);
+
+    let rotationkey = RLWEBlindRotationKey::generate(&sk, sampler, &mut rng);
+    let vec1 = vec![num1.clone(), num1];
+    let vec2 = vec![num2.clone(), num2];
+    let out1 =
+        comparison::greater_arbhcmp_fixed(&vec1, &vec2, 2, &rotationkey, DEELTA, HALF_DEELTA);
+
+    let a_mul_s = code
+        .iter()
+        .zip(out1.a())
+        .fold(FF::zero(), |acc, (&s_i, &a_i)| acc + s_i * a_i);
+    let decoded_value1 = decode(out1.b() - a_mul_s);
     //low_res恒为1，无误 equal_res为0 high_res 有问题
-    println!("{}",decoded_value1);
+    println!("{}", decoded_value1);
     /*
     let out_a=out.a();
     let mut num = FF::new(0);
@@ -92,50 +103,48 @@ fn main(){
     println!("{}",decoded_value);
     */
 
-
-/*    
-    let in1=comparison::rlwe_turn(num1,1);
-    let in2=comparison::rgsw_turn(num2,0);
-    let out1 = comparison::greater_hcmp(&in1,&in2);
-    let out2 = comparison::equality_hcmp(&in1, &in2);
-    let out3 = comparison::less_hcmp(&in1, &in2);
-
-    
-
-    let out1_a=out1.a();
-    let out2_a=out2.a();
-    let out3_a=out3.a();
+    /*
+        let in1=comparison::rlwe_turn(num1,1);
+        let in2=comparison::rgsw_turn(num2,0);
+        let out1 = comparison::greater_hcmp(&in1,&in2);
+        let out2 = comparison::equality_hcmp(&in1, &in2);
+        let out3 = comparison::less_hcmp(&in1, &in2);
 
 
-    let mut num1 = FF::new(0);
-    let mut num2 = FF::new(0);
-    let mut num3 = FF::new(0);
+
+        let out1_a=out1.a();
+        let out2_a=out2.a();
+        let out3_a=out3.a();
 
 
-    for i in 0..1024{
-        num1 = num1 +code[i]*out1_a[i];
-    }
-    for i in 0..1024{
-        num2 = num2 +code[i]*out2_a[i];
-    }
-    for i in 0..1024{
-        num3 = num3 +code[i]*out3_a[i];
-    }
+        let mut num1 = FF::new(0);
+        let mut num2 = FF::new(0);
+        let mut num3 = FF::new(0);
 
 
-    let decoded_value1 = decode(out1.b()-num1);
-    let decoded_value2 = decode(out2.b()-num2);
-    let decoded_value3 = decode(out3.b()-num3);
+        for i in 0..1024{
+            num1 = num1 +code[i]*out1_a[i];
+        }
+        for i in 0..1024{
+            num2 = num2 +code[i]*out2_a[i];
+        }
+        for i in 0..1024{
+            num3 = num3 +code[i]*out3_a[i];
+        }
 
 
-    println!("{}",decoded_value1);
-    println!("{}",decoded_value2);
-    println!("{}",decoded_value3);
-*/
+        let decoded_value1 = decode(out1.b()-num1);
+        let decoded_value2 = decode(out2.b()-num2);
+        let decoded_value3 = decode(out3.b()-num3);
 
+
+        println!("{}",decoded_value1);
+        println!("{}",decoded_value2);
+        println!("{}",decoded_value3);
+    */
 }
 
-/* 
+/*
 fn test_all(){
     //init
     let mut rng = thread_rng();
@@ -218,7 +227,7 @@ fn test_all(){
     println!("{:?}",in2_0);
 
     //gatebootstrapping have problems, so no test here
- 
+
 
     //test greater_hcmp:if in1_0's number > in2_0's number, then it will give back encrypted 1, otherwise -1(I use 2 and 1 to show)
     let out1 = comparison::greater_hcmp(&in1_0,&in2_0);
