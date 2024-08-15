5e17f987c8f4906632c93c8f31997003e31b27d6
Tianyu Zhang <andrew_sjtu@sjtu.edu.cn>
2024-08-15 18:56:09 -0500

    record

--- zkp/tests/test_range_check.rs
+++ zkp/tests/test_range_check.rs
@@ -1,13 +1,12 @@
 use algebra::{
     derive::{DecomposableField, Field, Prime},
-    DecomposableField, DenseMultilinearExtension, Field, FieldUniformSampler,
+    DenseMultilinearExtension, Field, FieldUniformSampler,
 };
-use num_traits::{One, Zero};
 use rand::prelude::*;
 use rand_distr::Distribution;
 use std::rc::Rc;
 use std::vec;
-use zkp::piop::{RangeCheck, RangeCheckInstance};
+use zkp::piop::{Lookup, LookupInstance};
 
 #[derive(Field, DecomposableField, Prime)]
 #[modulus = 132120577]
@@ -34,53 +33,39 @@ fn test_trivial_range_check() {
     let mut rng = thread_rng();
     let sampler = <FieldUniformSampler<FF>>::new();
 
+    // construct f
     let num_vars_f = 4;
     let f = Rc::new(DenseMultilinearExtension::from_evaluations_vec(
         num_vars_f,
         field_vec!(FF; 4, 6, 7, 2, 7, 0, 1, 6, 3, 2, 1, 0, 4, 1, 1, 6),
     ));
 
+    // construct t
     let num_vars_t = 3;
-    let range = 1 << num_vars_t;
     let t = Rc::new(DenseMultilinearExtension::from_evaluations_vec(
         num_vars_t,
         field_vec!(FF; 0, 1, 2, 3, 4, 5, 6, 7),
     ));
 
-    // compute the counting number m
-    let mut m_evaluations = vec![FF::zero(); 1 << num_vars_t];
-    f.iter().for_each(|x| {
-        let idx: usize = x.value() as usize;
-        m_evaluations[idx] += FF::one();
-    });
-    let m = DenseMultilinearExtension::from_evaluations_slice(num_vars_t, &m_evaluations);
-
+    // construct randomness
     let mut r = sampler.sample(&mut rng);
     while t[0] <= r && r <= t[(1 << num_vars_t) - 1] {
         r = sampler.sample(&mut rng);
     }
+    let mut u: Vec<_> = (0..num_vars_f).map(|_| sampler.sample(&mut rng)).collect();
+    u.push(r);
+    let randomness = u;
 
-    // compute t, inverse f and inverse t
-    let f_inverse: DenseMultilinearExtension<FF> = DenseMultilinearExtension::from_evaluations_vec(
-        num_vars_f,
-        f.iter().map(|x_f| FF::one() / (r - x_f)).collect(),
-    );
-    let t_inverse: DenseMultilinearExtension<FF> = DenseMultilinearExtension::from_evaluations_vec(
-        num_vars_t,
-        t.iter()
-            .zip(m.evaluations.iter())
-            .map(|(x_t, x_m)| *x_m / (r - x_t))
-            .collect(),
-    );
-
-    let instance = RangeCheckInstance::from_slice(&f, &f_inverse, &t_inverse, &m, range);
+    // construct instance
+    let instance = LookupInstance::from_slice(&f, &t);
     let info = instance.info();
 
-    let u: Vec<_> = (0..num_vars_f).map(|_| sampler.sample(&mut rng)).collect();
+    // prove
+    let (proof, oracle) = Lookup::prove(&instance, &randomness);
 
-    let proof = RangeCheck::prove(&instance, &u, r);
-    let subclaim = RangeCheck::verify(&proof, &info);
-    assert!(subclaim.verify_subclaim(f, &f_inverse, &t_inverse, &t, &m, &u, r, &info));
+    // verify
+    let subclaim = Lookup::verify(&proof, &info);
+    assert!(subclaim.verify_subclaim(f, t, oracle, &randomness, &info));
 }
 
 #[test]
@@ -92,6 +77,7 @@ fn test_random_range_check() {
     let num_vars_t = 6;
     let range = 1 << num_vars_t;
 
+    // construct f
     let f_evaluations: Vec<FF> = (0..(1 << num_vars_f))
         .map(|_| FF::new(rng.gen_range(0..range)))
         .collect();
@@ -100,44 +86,30 @@ fn test_random_range_check() {
         f_evaluations,
     ));
 
+    // construct t
     let t_evaluations: Vec<FF> = (0..range).map(FF::new).collect();
     let t = Rc::new(DenseMultilinearExtension::from_evaluations_vec(
         num_vars_t,
         t_evaluations,
     ));
 
-    // compute the counting number m
-    let mut m_evaluations = vec![FF::zero(); 1 << num_vars_t];
-    f.iter().for_each(|x| {
-        let idx: usize = x.value() as usize;
-        m_evaluations[idx] += FF::one();
-    });
-    let m = DenseMultilinearExtension::from_evaluations_slice(num_vars_t, &m_evaluations);
-
+    // construct randomness
     let mut r = sampler.sample(&mut rng);
     while t[0] <= r && r <= t[(1 << num_vars_t) - 1] {
         r = sampler.sample(&mut rng);
     }
+    let mut u: Vec<_> = (0..num_vars_f).map(|_| sampler.sample(&mut rng)).collect();
+    u.push(r);
+    let randomness = u;
 
-    // compute t, inverse f and inverse t
-    let f_inverse: DenseMultilinearExtension<FF> = DenseMultilinearExtension::from_evaluations_vec(
-        num_vars_f,
-        f.iter().map(|x_f| FF::one() / (r - x_f)).collect(),
-    );
-    let t_inverse: DenseMultilinearExtension<FF> = DenseMultilinearExtension::from_evaluations_vec(
-        num_vars_t,
-        t.iter()
-            .zip(m.evaluations.iter())
-            .map(|(x_t, x_m)| *x_m / (r - x_t))
-            .collect(),
-    );
-
-    let instance = RangeCheckInstance::from_slice(&f, &f_inverse, &t_inverse, &m, range as usize);
+    // construct instance
+    let instance = LookupInstance::from_slice(&f, &t);
     let info = instance.info();
 
-    let u: Vec<_> = (0..num_vars_f).map(|_| sampler.sample(&mut rng)).collect();
+    // prove
+    let (proof, oracle) = Lookup::prove(&instance, &randomness);
 
-    let proof = RangeCheck::prove(&instance, &u, r);
-    let subclaim = RangeCheck::verify(&proof, &info);
-    assert!(subclaim.verify_subclaim(f, &f_inverse, &t_inverse, &t, &m, &u, r, &info));
+    // verify
+    let subclaim = Lookup::verify(&proof, &info);
+    assert!(subclaim.verify_subclaim(f, t, oracle, &randomness, &info));
 }
