44c755a11284b380cf9e2d2a9dd9c5bc3456c334
Tianyu Zhang <andrew_sjtu@sjtu.edu.cn>
2024-08-16 23:25:05 -0700

    fmt

--- zkp/benches/range_check_bit_decomposition.rs
+++ zkp/benches/range_check_bit_decomposition.rs
@@ -47,47 +47,53 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         })
         .collect();
 
-    c.bench_function(&format!("prove"), |b| {
-        b.iter(|| {
+    c.bench_function(
+        &format!("prove lookup_num {} lookup size {}", lookup_num, num_vars),
+        |b| {
+            b.iter(|| {
+                let d_bits: Vec<_> = d
+                    .iter()
+                    .map(|x| x.get_decomposed_mles(base_len, bits_len))
+                    .collect();
+                let _: Vec<_> = d_bits.iter().collect();
+
+                let mut decomposed_bits = DecomposedBits::new(base, base_len, bits_len, num_vars);
+                for d_instance in d_bits.iter() {
+                    decomposed_bits.add_decomposed_bits_instance(d_instance);
+                }
+
+                let _ = decomposed_bits.info();
+
+                let u: Vec<_> = (0..num_vars).map(|_| uniform_ff.sample(&mut rng)).collect();
+                BitDecomposition::prove(&decomposed_bits, &u);
+            })
+        },
+    );
+
+    c.bench_function(
+        &format!("verify lookup_num {} lookup size {}", lookup_num, num_vars),
+        |b| {
             let d_bits: Vec<_> = d
                 .iter()
                 .map(|x| x.get_decomposed_mles(base_len, bits_len))
                 .collect();
-            let _: Vec<_> = d_bits.iter().collect();
+            let d_bits_ref: Vec<_> = d_bits.iter().collect();
 
             let mut decomposed_bits = DecomposedBits::new(base, base_len, bits_len, num_vars);
             for d_instance in d_bits.iter() {
                 decomposed_bits.add_decomposed_bits_instance(d_instance);
             }
 
-            let _ = decomposed_bits.info();
+            let decomposed_bits_info = decomposed_bits.info();
 
             let u: Vec<_> = (0..num_vars).map(|_| uniform_ff.sample(&mut rng)).collect();
-            BitDecomposition::prove(&decomposed_bits, &u);
-        })
-    });
-
-    c.bench_function(&format!("verify"), |b| {
-        let d_bits: Vec<_> = d
-            .iter()
-            .map(|x| x.get_decomposed_mles(base_len, bits_len))
-            .collect();
-        let d_bits_ref: Vec<_> = d_bits.iter().collect();
-
-        let mut decomposed_bits = DecomposedBits::new(base, base_len, bits_len, num_vars);
-        for d_instance in d_bits.iter() {
-            decomposed_bits.add_decomposed_bits_instance(d_instance);
-        }
-
-        let decomposed_bits_info = decomposed_bits.info();
-
-        let u: Vec<_> = (0..num_vars).map(|_| uniform_ff.sample(&mut rng)).collect();
-        let proof = BitDecomposition::prove(&decomposed_bits, &u);
-        b.iter(|| {
-            let subclaim = BitDecomposition::verifier(&proof, &decomposed_bits_info);
-            assert!(subclaim.verify_subclaim(&d, &d_bits_ref, &u, &decomposed_bits_info));
-        })
-    });
+            let proof = BitDecomposition::prove(&decomposed_bits, &u);
+            b.iter(|| {
+                let subclaim = BitDecomposition::verifier(&proof, &decomposed_bits_info);
+                assert!(subclaim.verify_subclaim(&d, &d_bits_ref, &u, &decomposed_bits_info));
+            })
+        },
+    );
 }
 
 fn configure() -> Criterion {
