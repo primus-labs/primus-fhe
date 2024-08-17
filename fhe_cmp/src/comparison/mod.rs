dedd0b38b1ce92582ec2096d06ae8414a461f26b
Arthur-hu-0806 <18710010806@163.com>
2024-08-17 15:27:58 +0800

    Update mod.rs

--- fhe_cmp/src/comparison/mod.rs
+++ fhe_cmp/src/comparison/mod.rs
@@ -1,10 +1,49 @@
 //!
 
-use algebra::{modulus::PowOf2Modulus, Field, NTTField, NTTPolynomial, Polynomial, FieldDiscreteGaussianSampler, Basis};
-use fhe_core::{lwe_modulus_switch, ModulusSwitchRoundMethod, RLWEBlindRotationKey};
+use algebra::{derive::{DecomposableField, FheField, Field, Prime, NTT}, modulus::PowOf2Modulus, Field, NTTField, NTTPolynomial, Polynomial, FieldDiscreteGaussianSampler, Basis};
+use fhe_core::{BlindRotationType, ConstParameters,ModulusSwitchRoundMethod, Parameters,
+    RingSecretKeyType, SecretKeyType, StepsAfterBR,lwe_modulus_switch,RLWEBlindRotationKey};
 use lattice::{LWE, RGSW, RLWE};
 use rand::prelude::*;
+use once_cell::sync::Lazy;
 
+#[derive(Field, Prime, DecomposableField, FheField, NTT)]
+#[modulus = 132120577]
+///default size
+pub struct Default(pub u64);
+/// inner type
+pub type Inner = u64;
+/// ciphertext space
+pub const FP: Inner = 132120577;
+/// message space
+pub const FT: Inner = 16;
+///encode of 1
+pub const DEELTA: Default = Default((FP as f64 / FT as f64) as Inner);
+///encode of 1/2
+pub const HALF_DEELTA: Default = Default((FP as f64 / (FT as f64 * 2.0)) as Inner);
+
+/// Default 128-bits security Parameters
+pub static DEFAULT_PARAMERTERS: Lazy<Parameters<u64, Default>> =
+    Lazy::new(|| {
+        Parameters::<u64, Default>::new(ConstParameters {
+            lwe_dimension: 1024,
+            lwe_modulus: 2048,
+            t: 16,
+            lwe_noise_std_dev: 3.20,
+            secret_key_type: SecretKeyType::Binary,
+            blind_rotation_type: BlindRotationType::RLWE,
+            ring_dimension: 1024,
+            ring_modulus: 132120577,
+            ring_noise_std_dev: 3.20 * ((1 << 1) as f64),
+            ring_secret_key_type: RingSecretKeyType::Binary,
+            blind_rotation_basis_bits: 1,
+            steps_after_blind_rotation: StepsAfterBR::Ms,
+            key_switching_basis_bits: 1,
+            key_switching_std_dev: 3.2 * ((1 << 1) as f64),
+            modulus_switcing_round_method: ModulusSwitchRoundMethod::Round,
+        })
+        .unwrap()
+    });
 
 /// Performs the initialization, turning 2 numbers to their corresponding ciphertext
 pub fn initial<F,R>(
@@ -15,7 +54,6 @@ pub fn initial<F,R>(
     error_sampler: FieldDiscreteGaussianSampler,
     mut rng: R,
     basis: Basis<F>,
-    poly_length: usize,
     half_delta: F
 ) -> (Vec<RLWE<F>>, Vec<RGSW<F>>)
 where
@@ -72,9 +110,31 @@ where
         error_sampler,
         &mut rng,
         basis,
-        poly_length,
+        ring_dimension,
     );
     (vec1,vec2)
+}
+
+/// decrypt
+pub fn decrypt<F: Field<Value = u64> + NTTField>(
+    sk: &[F],
+    ciphertext: LWE<F> 
+) -> u64
+{
+    let a_mul_s = sk
+        .iter()
+        .zip(ciphertext.a())
+        .fold(F::zero(), |acc, (&s, &a)| acc + s * a);
+    let outcome = decode(ciphertext.b() - a_mul_s);
+    outcome
+}
+
+/// Peforms the operation turning a value to its real number
+pub fn decode<F: Field<Value = u64> + NTTField>(
+    c: F
+) -> u64 
+{
+    (c.value() as f64 * 16 as f64 / 132120577 as f64).round() as u64 % 16
 }
 
 /// Performs the operation of turning a vector to the corresponding RGSW ciphertext.
