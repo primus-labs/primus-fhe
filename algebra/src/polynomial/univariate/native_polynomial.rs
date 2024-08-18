01566cc158ad49765c5834dff9452d936f4379fb
Xiang Xie <xiexiangiscas@gmail.com>
2024-08-18 08:00:03 +0800

    fix rng apis

--- algebra/src/polynomial/univariate/native_polynomial.rs
+++ algebra/src/polynomial/univariate/native_polynomial.rs
@@ -187,34 +187,34 @@ impl<F: Field> Polynomial<F> {
 
     /// Generate a random binary [`Polynomial<F>`].
     #[inline]
-    pub fn random_with_binary<R>(n: usize, mut rng: R) -> Self
+    pub fn random_with_binary<R>(n: usize, rng: &mut R) -> Self
     where
         R: Rng + CryptoRng,
     {
-        Self::new(crate::utils::sample_binary_field_vec(n, &mut rng))
+        Self::new(crate::utils::sample_binary_field_vec(n, rng))
     }
 
     /// Generate a random ternary [`Polynomial<F>`].
     #[inline]
-    pub fn random_with_ternary<R>(n: usize, mut rng: R) -> Self
+    pub fn random_with_ternary<R>(n: usize, rng: &mut R) -> Self
     where
         R: Rng + CryptoRng,
     {
-        Self::new(crate::utils::sample_ternary_field_vec(n, &mut rng))
+        Self::new(crate::utils::sample_ternary_field_vec(n, rng))
     }
 
     /// Generate a random [`Polynomial<F>`] with discrete gaussian distribution.
     #[inline]
     pub fn random_with_gaussian<R>(
         n: usize,
-        mut rng: R,
+        rng: &mut R,
         gaussian: FieldDiscreteGaussianSampler,
     ) -> Self
     where
         R: Rng + CryptoRng,
     {
         if gaussian.cbd_enable() {
-            Self::new(crate::utils::sample_cbd_field_vec(n, &mut rng))
+            Self::new(crate::utils::sample_cbd_field_vec(n, rng))
         } else {
             Self::new(gaussian.sample_iter(rng).take(n).collect())
         }
@@ -222,7 +222,7 @@ impl<F: Field> Polynomial<F> {
 
     /// Generate a random [`Polynomial<F>`].
     #[inline]
-    pub fn random<R>(n: usize, rng: R) -> Self
+    pub fn random<R>(n: usize, rng: &mut R) -> Self
     where
         R: Rng + CryptoRng,
     {
@@ -236,7 +236,7 @@ impl<F: Field> Polynomial<F> {
 
     /// Generate a random [`Polynomial<F>`] with a specified distribution `dis`.
     #[inline]
-    pub fn random_with_distribution<R, D>(n: usize, rng: R, distribution: D) -> Self
+    pub fn random_with_distribution<R, D>(n: usize, rng: &mut R, distribution: D) -> Self
     where
         R: Rng + CryptoRng,
         D: Distribution<F>,
