4993d604da98307d06ef05a2cfcba3c0fa7b1c9e
Arthur-hu-0806 <18710010806@163.com>
2024-08-20 11:03:13 +0800

    Update parameters.rs

--- fhe_cmp/src/comparison/parameters.rs
+++ fhe_cmp/src/comparison/parameters.rs
@@ -1,7 +1,8 @@
-
 use algebra::derive::{DecomposableField, FheField, Field, Prime, NTT};
-use fhe_core::{BlindRotationType, ConstParameters,ModulusSwitchRoundMethod, Parameters,
-    RingSecretKeyType, SecretKeyType, StepsAfterBR};
+use fhe_core::{
+    BlindRotationType, ConstParameters, ModulusSwitchRoundMethod, Parameters, RingSecretKeyType,
+    SecretKeyType, StepsAfterBR,
+};
 use once_cell::sync::Lazy;
 
 #[derive(Field, Prime, DecomposableField, FheField, NTT)]
@@ -20,24 +21,23 @@ pub const DELTA: Default = Default((FP as f64 / FT as f64) as Inner);
 pub const HALF_DELTA: Default = Default((FP as f64 / (FT as f64 * 2.0)) as Inner);
 
 /// Default 128-bits security Parameters
-pub static DEFAULT_PARAMERTERS: Lazy<Parameters<u64, Default>> =
-    Lazy::new(|| {
-        Parameters::<u64, Default>::new(ConstParameters {
-            lwe_dimension: 1024,
-            lwe_modulus: 2048,
-            t: 16,
-            lwe_noise_std_dev: 3.20,
-            secret_key_type: SecretKeyType::Binary,
-            blind_rotation_type: BlindRotationType::RLWE,
-            ring_dimension: 1024,
-            ring_modulus: 132120577,
-            ring_noise_std_dev: 3.20 * ((1 << 1) as f64),
-            ring_secret_key_type: RingSecretKeyType::Binary,
-            blind_rotation_basis_bits: 1,
-            steps_after_blind_rotation: StepsAfterBR::Ms,
-            key_switching_basis_bits: 1,
-            key_switching_std_dev: 3.2 * ((1 << 1) as f64),
-            modulus_switcing_round_method: ModulusSwitchRoundMethod::Round,
-        })
-        .unwrap()
-    });
+pub static DEFAULT_PARAMERTERS: Lazy<Parameters<u64, Default>> = Lazy::new(|| {
+    Parameters::<u64, Default>::new(ConstParameters {
+        lwe_dimension: 1024,
+        lwe_modulus: 2048,
+        t: 16,
+        lwe_noise_std_dev: 3.20,
+        secret_key_type: SecretKeyType::Binary,
+        blind_rotation_type: BlindRotationType::RLWE,
+        ring_dimension: 1024,
+        ring_modulus: 132120577,
+        ring_noise_std_dev: 3.20 * ((1 << 1) as f64),
+        ring_secret_key_type: RingSecretKeyType::Binary,
+        blind_rotation_basis_bits: 1,
+        steps_after_blind_rotation: StepsAfterBR::Ms,
+        key_switching_basis_bits: 1,
+        key_switching_std_dev: 3.2 * ((1 << 1) as f64),
+        modulus_switcing_round_method: ModulusSwitchRoundMethod::Round,
+    })
+    .unwrap()
+});
