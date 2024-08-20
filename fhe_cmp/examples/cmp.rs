981210e227a1478d9a223c6ebe9906c3125d3d36
Arthur-hu-0806 <18710010806@163.com>
2024-08-20 11:12:59 +0800

    change rotationkey place

--- fhe_cmp/examples/cmp.rs
+++ fhe_cmp/examples/cmp.rs
@@ -1,4 +1,4 @@
-use fhe_cmp::comparison::{self, decrypt, encrypt, DELTA, HALF_DELTA,Compare};
+use fhe_cmp::comparison::{self, decrypt, encrypt, Compare, DELTA, HALF_DELTA};
 use fhe_core::{RLWEBlindRotationKey, SecretKeyPack};
 use rand::prelude::*;
 fn main() {
@@ -8,42 +8,62 @@ fn main() {
     let basis = param.blind_rotation_basis();
     let poly_length = param.ring_dimension();
     let sampler = param.ring_noise_distribution();
-    let rlwe_sk = sk.ring_secret_key().as_slice();  
-for _ in 0..50{
+    let rlwe_sk = sk.ring_secret_key().as_slice();
     let rotationkey = Compare::new(&RLWEBlindRotationKey::generate(&sk, sampler, &mut rng));
-    let x = rng.gen_range(0..100000);
-    let y = rng.gen_range(0..100000);
-    let (value1,value2) = encrypt(
-        x, 
-        y, 
-        param.ring_dimension(),
-        sk.ntt_ring_secret_key(),
-        sampler,
-        &mut rng,
-        basis,
-        DELTA,
-    );
-    let out1 = Compare::greater_arbhcmp(&rotationkey,&value1, &value2, DELTA, HALF_DELTA, poly_length);
-    let out2 = Compare::less_arbhcmp(&rotationkey, &value1, &value2, DELTA, HALF_DELTA, poly_length);
-    let out3 = Compare::equality_arbhcmp(&rotationkey, &value1, &value2, poly_length, DELTA);
-    let decoded_value1 = decrypt(rlwe_sk, out1);
-    let decoded_value2 = decrypt(rlwe_sk, out2);
-    let decoded_value3 = decrypt(rlwe_sk, out3);
-    if x > y {
-        if decoded_value1 != 1 || decoded_value2 != param.t() - 1 || decoded_value3 != param.t() - 1{
-            println!("error1");
-        }
-    }else if x < y {
-        if decoded_value1 != param.t() - 1 || decoded_value2 != 1 || decoded_value3 != param.t() - 1{
-            println!("error2");
-        }
-    }else{
-        if decoded_value1 != param.t() - 1 || decoded_value2 != param.t() - 1 || decoded_value3 != 1{
-            println!("error3");
+    for _ in 0..50 {
+        let x = rng.gen_range(0..100000);
+        let y = rng.gen_range(0..100000);
+        let (value1, value2) = encrypt(
+            x,
+            y,
+            param.ring_dimension(),
+            sk.ntt_ring_secret_key(),
+            sampler,
+            &mut rng,
+            basis,
+            DELTA,
+        );
+        let out1 = Compare::greater_arbhcmp(
+            &rotationkey,
+            &value1,
+            &value2,
+            DELTA,
+            HALF_DELTA,
+            poly_length,
+        );
+        let out2 = Compare::less_arbhcmp(
+            &rotationkey,
+            &value1,
+            &value2,
+            DELTA,
+            HALF_DELTA,
+            poly_length,
+        );
+        let out3 = Compare::equality_arbhcmp(&rotationkey, &value1, &value2, poly_length, DELTA);
+        let decoded_value1 = decrypt(rlwe_sk, out1);
+        let decoded_value2 = decrypt(rlwe_sk, out2);
+        let decoded_value3 = decrypt(rlwe_sk, out3);
+        if x > y {
+            if decoded_value1 != 1
+                || decoded_value2 != param.t() - 1
+                || decoded_value3 != param.t() - 1
+            {
+                println!("error1");
+            }
+        } else if x < y {
+            if decoded_value1 != param.t() - 1
+                || decoded_value2 != 1
+                || decoded_value3 != param.t() - 1
+            {
+                println!("error2");
+            }
+        } else {
+            if decoded_value1 != param.t() - 1
+                || decoded_value2 != param.t() - 1
+                || decoded_value3 != 1
+            {
+                println!("error3");
+            }
         }
     }
 }
-
-
-}
-
