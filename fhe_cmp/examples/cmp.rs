ab7154d762dc0a60d0bdf53e3cf18a9c0ef8d98a
Arthur-hu-0806 <18710010806@163.com>
2024-08-20 10:43:23 +0800

    Update cmp.rs

--- fhe_cmp/examples/cmp.rs
+++ fhe_cmp/examples/cmp.rs
@@ -1,4 +1,4 @@
-use fhe_cmp::comparison::{self, decrypt, initial, DELTA, HALF_DELTA};
+use fhe_cmp::comparison::{self, decrypt, encrypt, DELTA, HALF_DELTA,Compare};
 use fhe_core::{RLWEBlindRotationKey, SecretKeyPack};
 use rand::prelude::*;
 fn main() {
@@ -10,10 +10,10 @@ fn main() {
     let sampler = param.ring_noise_distribution();
     let rlwe_sk = sk.ring_secret_key().as_slice();  
 for _ in 0..50{
-    let rotationkey = RLWEBlindRotationKey::generate(&sk, sampler, &mut rng);
+    let rotationkey = Compare::new(&RLWEBlindRotationKey::generate(&sk, sampler, &mut rng));
     let x = rng.gen_range(0..100000);
     let y = rng.gen_range(0..100000);
-    let (value1,value2) = initial(
+    let (value1,value2) = encrypt(
         x, 
         y, 
         param.ring_dimension(),
@@ -23,22 +23,22 @@ for _ in 0..50{
         basis,
         DELTA,
     );
-    let out1 = comparison::greater_arbhcmp(&value1, &value2, &rotationkey, DELTA, HALF_DELTA, poly_length);
-    let out2 = comparison::less_arbhcmp(&value1, &value2, &rotationkey, DELTA, HALF_DELTA, poly_length);
-    let out3 = comparison::equality_arbhcmp(&value1, &value2, &rotationkey, poly_length, DELTA);
+    let out1 = Compare::greater_arbhcmp(&rotationkey,&value1, &value2, DELTA, HALF_DELTA, poly_length);
+    let out2 = Compare::less_arbhcmp(&rotationkey, &value1, &value2, DELTA, HALF_DELTA, poly_length);
+    let out3 = Compare::equality_arbhcmp(&rotationkey, &value1, &value2, poly_length, DELTA);
     let decoded_value1 = decrypt(rlwe_sk, out1);
     let decoded_value2 = decrypt(rlwe_sk, out2);
     let decoded_value3 = decrypt(rlwe_sk, out3);
     if x > y {
-        if decoded_value1 != 1 || decoded_value2 != 15 || decoded_value3 != 15{
+        if decoded_value1 != 1 || decoded_value2 != param.t() - 1 || decoded_value3 != param.t() - 1{
             println!("error1");
         }
     }else if x < y {
-        if decoded_value1 != 15 || decoded_value2 != 1 || decoded_value3 != 15{
+        if decoded_value1 != param.t() - 1 || decoded_value2 != 1 || decoded_value3 != param.t() - 1{
             println!("error2");
         }
     }else{
-        if decoded_value1 != 15 || decoded_value2 != 15 || decoded_value3 != 1{
+        if decoded_value1 != param.t() - 1 || decoded_value2 != param.t() - 1 || decoded_value3 != 1{
             println!("error3");
         }
     }
