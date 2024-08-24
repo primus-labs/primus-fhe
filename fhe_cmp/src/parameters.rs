ff9e7d7b5c821526772d08a88aa5d4066d029e0d
Arthur-hu-0806 <18710010806@163.com>
2024-08-24 19:35:24 +0800

    change

--- fhe_cmp/src/parameters.rs
+++ fhe_cmp/src/parameters.rs
@@ -1,7 +1,7 @@
 use algebra::derive::{DecomposableField, FheField, Field, Prime, NTT};
 use fhe_core::{
-    BlindRotationType, ConstParameters, ModulusSwitchRoundMethod, Parameters, RingSecretKeyType,
-    SecretKeyType, StepsAfterBR,
+    BlindRotationType, ConstParameters, LWESecretKeyType,
+    ModulusSwitchRoundMethod, Parameters, RingSecretKeyType, Steps,
 };
 use once_cell::sync::Lazy;
 
@@ -24,20 +24,20 @@ pub const HALF_DELTA: FF = FF((FP as f64 / (FT as f64 * 2.0)) as Inner);
 pub static DEFAULT_PARAMERTERS: Lazy<Parameters<u64, FF>> = Lazy::new(|| {
     Parameters::<u64, FF>::new(ConstParameters {
         lwe_dimension: 1024,
-        lwe_modulus: 2048,
-        t: 16,
-        lwe_noise_std_dev: 3.20,
-        secret_key_type: SecretKeyType::Binary,
+        lwe_cipher_modulus: 2048,
+        lwe_plain_modulus: 16,
+        lwe_noise_standard_deviation: 3.20,
+        lwe_secret_key_type: LWESecretKeyType::Binary,
         blind_rotation_type: BlindRotationType::RLWE,
         ring_dimension: 1024,
         ring_modulus: 132120577,
-        ring_noise_std_dev: 3.20 * ((1 << 1) as f64),
+        ring_noise_standard_deviation: 3.20 * ((1 << 1) as f64),
         ring_secret_key_type: RingSecretKeyType::Binary,
         blind_rotation_basis_bits: 1,
-        steps_after_blind_rotation: StepsAfterBR::Ms,
         key_switching_basis_bits: 1,
-        key_switching_std_dev: 3.2 * ((1 << 1) as f64),
-        modulus_switcing_round_method: ModulusSwitchRoundMethod::Round,
+        key_switching_standard_deviation: 3.2 * ((1 << 1) as f64),
+        modulus_switching_round_method: ModulusSwitchRoundMethod::Round,
+        steps: Steps::BrMs,
     })
     .unwrap()
 });
