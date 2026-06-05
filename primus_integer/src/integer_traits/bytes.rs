dc1f0af0c7ce9c674211ffc112a98373f613535a
renzhangLiu <liurenzhang@amss.ac.cn>
2026-06-05 16:32:18 +0800

    remove tests for Bits and ByteCount

--- primus_integer/src/integer_traits/bytes.rs
+++ primus_integer/src/integer_traits/bytes.rs
@@ -17,30 +17,3 @@ macro_rules! impl_bytes {
 impl_bytes!(
     i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize
 );
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    macro_rules! test_bytes_per_type {
-        ($($T:ty),*; $($W:literal),*) => {
-            $(
-                assert_eq!(<$T as ByteCount>::BYTES, $W);
-            )*
-        };
-    }
-
-    #[test]
-    fn test_bytes() {
-        #[cfg(target_pointer_width = "32")]
-        test_bytes_per_type!(
-            i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize;
-             1,  1,   2,   2,   4,   4,   8,   8,   16,   16,     4,     4
-        );
-        #[cfg(target_pointer_width = "64")]
-        test_bytes_per_type!(
-            i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize;
-             1,  1,   2,   2,   4,   4,   8,   8,   16,   16,     8,     8
-        );
-    }
-}
