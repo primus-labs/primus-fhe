54634c9f77de67df4c0d574a41510405f8806d80
renzhangLiu <liurenzhang@amss.ac.cn>
2026-06-05 20:51:56 +0800

    test: add unit tests for traits Size (#176)

--- primus_integer/src/size.rs
+++ primus_integer/src/size.rs
@@ -42,3 +42,44 @@ impl<T: ByteCount> Size for Arc<[T]> {
         self.len() * T::BYTES
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! test_size_per_type {
+		($($T:ty),*; $($W:literal),*) => {
+			$(
+				let v : Vec<$T> = Vec::<$T>::from([1,2,3,4]);
+                assert_eq!(v.byte_count(), $W*4);
+
+				let slice_v : &[$T] = &Vec::<$T>::from([1,2,3,4]);
+                assert_eq!(slice_v.byte_count(), $W*4);
+
+				let boxed: Box<[$T]> = Vec::<$T>::from([1,2,3,4]).into_boxed_slice();
+                assert_eq!(boxed.byte_count(), $W*4);
+
+				let arr = [1 as $T; 4];
+                assert_eq!(arr.byte_count(), $W*4);
+
+				let a: Arc<[$T]> = Arc::from([1 as $T,2,3,4]);
+                assert_eq!(a.byte_count(), $W*4);
+
+			)*
+		};
+	}
+
+    #[test]
+    fn test_size() {
+        #[cfg(target_pointer_width = "32")]
+        test_size_per_type!(
+            i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize;
+             1,  1,   2,   2,   4,   4,   8,   8,   16,   16,     4,     4
+        );
+        #[cfg(target_pointer_width = "64")]
+        test_size_per_type!(
+            i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize;
+             1,  1,   2,   2,   4,   4,   8,   8,   16,   16,     8,     8
+        );
+    }
+}
