d723bef3afe6d55300e855f54d84b05a9b0f6bd0
renzhangLiu <liurenzhang@amss.ac.cn>
2026-06-05 11:25:17 +0800

    add tests to traits (integer_traits/)Bits, ByteCount, Size

--- primus_integer/src/size.rs
+++ primus_integer/src/size.rs
@@ -42,3 +42,35 @@ impl<T: ByteCount> Size for Arc<[T]> {
         self.len() * T::BYTES
     }
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+	macro_rules! test_size_per_type {
+		($($T:ty),*; $($W:literal),*) => {
+			$(
+				assert_eq!(<$T as ByteCount>::BYTES, $W); 
+			)*
+		};
+	}
+
+
+    #[test]
+    fn test_size() {
+		#[cfg(target_pointer_width = "32")]
+		test_size_per_type!(
+			i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize;
+			 1,  1,   2,   2,   4,   4,   8,   8,   16,   16,     4,     4
+		);
+		#[cfg(target_pointer_width = "64")]
+		test_size_per_type!(
+			i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize;
+			 1,  1,   2,   2,   4,   4,   8,   8,   16,   16,     8,     8
+		);
+    }
+
+}
+
